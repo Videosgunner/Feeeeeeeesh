46a6d5562b03f495682e53dab54354a1de238de6
Videosgunner <159513438+Videosgunner@users.noreply.github.com>
2024-04-07 14:20:47 -0700

    Add files via upload

---
+++ fish.js
@@ -0,0 +1,64 @@
+class Fish  {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.size = 15;
+    this.speed = 5;
+
+    this.dx = 0;
+    this.dy = 0;
+
+    this.waterFriction = 0.96;
+    this.stamina = 200;
+    this.color = random(15, 60);
+    this.saturation = 100;
+    this.offset = floor(random(0, this.stamina-1));
+    this.noMove = false;
+  }
+
+  drawSelf() {
+    //no outline 
+    noStroke(); 
+    //get direction fish is facing 
+    var angle = atan2(this.dy, this.dx); 
+    //transform the fish to face the right direction 
+    push(); 
+    translate(this.x, this.y); 
+    rotate(angle); 
+    //draw an orange ellipse and triangle to represent the fish 
+    fill(this.color, this.saturation, 100); 
+    ellipse(0, 0, this.size * 1.25 * (this.speed / 5), this.size); 
+    triangle(-this.size/2 * (this.speed / 5), 0, -this.size  * (this.speed / 5), -this.size/3, -this.size  * (this.speed / 5), this.size/3); 
+    fill(0);
+    ellipse(this.size/3 * (this.speed / 5), 0, this.size/4, this.size/4) 
+    pop(); 
+    //end transformation
+  }
+
+  mutate() {
+    this.size *= 1 - (random(-2, 2)/10);
+    this.speed *= 1 - (random(-2, 2)/10);
+    this.stamina *= 1 - (random(-2, 2)/10);
+    this.stamina = floor(this.stamina)
+  }
+
+  move() {
+    if (frameCount % this.stamina == this.offset) {
+      var vel_angle = random(0, 360);
+      this.dx = cos(vel_angle) * this.speed;
+      this.dy = sin(vel_angle) * this.speed;
+    }
+
+    if (this.x + this.dx < 0 || this.x + this.dx > width || this.y + this.dy < 0 || this.y + this.dy > height) {
+      this.dx *= -1
+      this.dy *= -1
+    } else {
+      this.x += this.dx;
+      this.y += this.dy;
+    }
+
+    this.dx *= this.waterFriction;
+    this.dy *= this.waterFriction;
+  }
+}
---
+++ script.js
@@ -0,0 +1,88 @@
+var target = 0;
+var mode = null;
+
+fishlist = []
+
+function setup() {
+  var main = createCanvas(windowWidth - 220, windowHeight);
+  main.position(220,0);
+  colorMode(HSB, 100);
+
+  while (fishlist.length < 5) {
+    fish = new Fish(random(0, width), random(0, height));
+    fish.mutate();
+    fishlist.push(fish);
+  }
+
+}
+
+function draw() {
+  background(60+(cos(frameCount/200)*2), 70, 70);
+  for (var eachfish of fishlist) {
+    eachfish.drawSelf();
+    if (eachfish.noMove == false) {
+      eachfish.move();
+    }
+  }
+
+  for (var fish of fishlist) {
+    if (fish.x - ((fish.size * fish.speed / 5) / 2) < mouseX && fish.x + ((fish.size * fish.speed / 5) / 2) > mouseX && fish.y - fish.size / 2 < mouseY && fish.y + fish.size / 2 > mouseY) {
+      fish.saturation = 50;
+    } else {
+      fish.saturation = 100;
+    }
+  }
+
+  if (target != 0) {
+    target.noMove = true;
+    target.x = mouseX;
+    target.y = mouseY;
+  }
+
+  
+
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    push();
+    translate(mouseX, mouseY);
+    noCursor();
+    stroke(3);
+    fill(0, 0, 100);
+    triangle(0,0,4,10,10,4);
+    pop();
+  }
+  
+  switch (mode) {
+    case 2:
+      if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+        document.getElementById("contentP").innerHTML = "Position: (" + mouseX + ", " + floor(mouseY) + ")";
+        
+      } else {
+        document.getElementById("contentP").innerHTML = "Position: NA";
+      }
+      break
+    case 847:
+      document.getElementById("contentP").innerHTML = "Fish with SPICE.";
+      break
+    default:
+      document.getElementById("contentP").innerHTML = "";
+  }
+
+}
+
+function mouseDragged() {
+  for (var fish of fishlist) {
+    if (fish.x - ((fish.size * fish.speed / 5) / 2) < mouseX && fish.x + ((fish.size * fish.speed / 5) / 2) > mouseX && fish.y - fish.size / 2 < mouseY && fish.y + fish.size / 2 > mouseY && target === 0) {
+      target = fish;
+    }
+  }
+}
+
+function mouseReleased() {
+  for (var fish of fishlist) {
+    fish.noMove = false;
+  }
+  target.dx = mouseX-pmouseX;
+  target.dy = mouseY-pmouseY;
+  target = 0;
+
+}
