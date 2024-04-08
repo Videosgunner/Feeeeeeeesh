3054ecc9d8beefd6ae9a53abdddebac00705a321
Videosgunner <159513438+Videosgunner@users.noreply.github.com>
2024-04-07 22:22:34 -0700

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,5 +1,7 @@
 function initializeButtons() {
+  if (mode != 0) {
   var namefield = document.createElement("input");
   namefield.id = "namefield";
   document.getElementById("contentButtons").appendChild(namefield);
+  }
 }
