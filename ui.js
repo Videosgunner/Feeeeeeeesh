c47b20548564dd0ab1df89c5514a7233c17d1f83
Videosgunner <159513438+Videosgunner@users.noreply.github.com>
2024-04-07 21:51:36 -0700

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,5 @@
+function initializeButtons() {
+  var namefield = document.createElement("input");
+  namefield.id = "namefield";
+  document.getElementById("contentButtons").appendChild(namefield);
+}
