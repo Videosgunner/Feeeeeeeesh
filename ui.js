b0b2f8855a0842110cb891c89b2e0167610e1b48
Videosgunner <159513438+Videosgunner@users.noreply.github.com>
2024-04-08 17:18:40 -0700

    Update ui.js

--- ui.js
+++ ui.js
@@ -2,6 +2,10 @@ function initializeButtons() {
   if (document.getElementById("contentButtons").hasChildNodes() == false) {
   var namefield = document.createElement("input");
   namefield.id = "namefield";
+  var namelabel = document.createElement("label");
+  namelabel.for = "namefield";
+  namelabel.innerHTML = "Name Fish:"
+  document.getElementById("contentButtons").appendChild(namelabel);
   document.getElementById("contentButtons").appendChild(namefield);
   }
 }
